90fcaf3bfa108ee698256d711f20d47b95ba8525
eglogicsabhishek1 <kumar.abhishek@eglogics.com>
2025-02-12 16:29:24 +0530

    Add files via upload

---
+++ Abhishek/django/medical_website/cms/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render,redirect
+from .models import Prescription
+
+from .forms import PrescriptionForm
+
+def homepage(request):
+    return render(request, 'cms/home.html')
+
+def about(request):
+    return render(request, 'cms/about.html')
+
+def contact(request):
+    return render(request, 'cms/contact.html')
+
+
+def prescriptions_list(request):
+    prescriptions = Prescription.objects.all()
+    return render(request, 'cms/prescriptions_list.html', {'prescriptions': prescriptions})
+
+
+def add_prescription(request):
+    if request.method == 'POST':
+        form = PrescriptionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('prescriptions_list')
+    else:
+        form = PrescriptionForm()
+    return render(request, 'cms/add_prescription.html', {'form': form})
+
+def chatbot(request):
+    return render(request, 'cms/chatbot.html')
