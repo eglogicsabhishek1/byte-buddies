f86bae8d45a92e8cf5414fef7d3d2d05bb6f7b73
eglogicsabhishek1 <kumar.abhishek@eglogics.com>
2025-02-12 19:02:22 +0530

    Add files via upload

--- Abhishek/django/medical_website/cms/models.py
+++ Abhishek/django/medical_website/cms/models.py
@@ -8,3 +8,14 @@ class Prescription(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)  
+    number = models.CharField(max_length=15)  
+    email = models.EmailField()  
+    dob=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name  
